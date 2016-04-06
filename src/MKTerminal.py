458a6301d2513e951a9808c03440400f60fa39fb
Matthias Kastner <mail@matthias-kastner.de>
2016-04-06 16:54:37 +0200

    white space correction

--- src/MKTerminal.py
+++ src/MKTerminal.py
@@ -3,88 +3,87 @@
 import sys, os, threading, time
 
 if sys.version_info >= (3, 0):
-	def character(b):
-		return b.decode('latin1')
+    def character(b):
+        return b.decode('latin1')
 else:
-	def character(b):
-		return b
+    def character(b):
+        return b
 
 class MKConsole(object):
-	def __init__(self):
-		self.fd = sys.stdin.fileno()
+    def __init__(self):
+        self.fd = sys.stdin.fileno()
 
-	def getkey(self):
-		c = os.read(self.fd, 1)
-		return c
+        def getkey(self):
+            c = os.read(self.fd, 1)
+                return c
 
 class MKTerminal(object):
-	EXITCHARCTER = 'q'
-	message = ''
-	alive=False
-	readyToSend=False
-	readyToDisplay=False
+    EXITCHARCTER = 'q'
+    message = ''
+    alive=False
+    readyToSend=False
+    readyToDisplay=False
 
-	def __init__(self,echo=False):
-		self.echo = echo
-		self.Console = MKConsole()
+    def __init__(self,echo=False):
+        self.echo = echo
+        self.Console = MKConsole()
 
-	def start(self):
-		self.alive = True
-		self.readerthread = threading.Thread(target=self.read)
-		self.readerthread.setDaemon(True) # never care about it anymore
-		self.readerthread.start()
-		self.writerthread = threading.Thread(target=self.write)
-		self.writerthread.setDaemon(True) # never care about it anymore
-		self.writerthread.start()
+    def start(self):
+        self.alive = True
+        self.readerthread = threading.Thread(target=self.read)
+        self.readerthread.setDaemon(True) # never care about it anymore
+        self.readerthread.start()
+        self.writerthread = threading.Thread(target=self.write)
+        self.writerthread.setDaemon(True) # never care about it anymore
+        self.writerthread.start()
 
-	def join(self):
-		self.readerthread.join()
+    def join(self):
+        self.readerthread.join()
 
-	def stop(self):
-		self.alive = False
+    def stop(self):
+        self.alive = False
 
+    def send(self,myChar):
+        if myChar == '\n':
+            self.readyToSend=True
+        else:
+            self.message+=myChar
 
-	def send(self,myChar):
-		if myChar == '\n':
-			self.readyToSend=True
-		else:
-			self.message+=myChar
+    def read(self):
+        while self.alive:
+            try:
+                time.sleep(0.1)
+                b = self.Console.getkey()
+                c = character(b)
+                if c == self.EXITCHARCTER:
+                    raise KeyboardInterrupt
+                self.send(c)
+                if self.echo == True:
+                    sys.stdout.write(c)
+                    sys.stdout.flush()
+            except KeyboardInterrupt:
+                self.stop()
 
-	def read(self):
-		while self.alive:
-			try:
-                            time.sleep(0.1)
-                            b = self.Console.getkey()
-                            c = character(b)
-                            if c == self.EXITCHARCTER:
-                                raise KeyboardInterrupt
-                            self.send(c)
-                            if self.echo == True:
-                                sys.stdout.write(c)
-                                sys.stdout.flush()
-                        except KeyboardInterrupt:
-                                self.stop()
+    def write(self):
+        while self.alive:
+            if self.readyToDisplay:
+                sys.stdout.write(self.printme)
+                sys.stdout.flush()
+                self.readyToDisplay=False
+            time.sleep(0.1)
 
-        def write(self):
-                while self.alive:
-                        if self.readyToDisplay:
-                                sys.stdout.write(self.printme)
-                                sys.stdout.flush()
-                                self.readyToDisplay=False
-                        time.sleep(0.1)
+    def isAlive(self):
+        return self.alive
 
-        def isAlive(self):
-                return self.alive
+    def isReady(self):
+        return self.readyToSend
 
-        def isReady(self):
-                return self.readyToSend
+    def getMessage(self):
+        message=self.message
+        self.message=''
+        self.readyToSend=False
+        return message
 
-        def getMessage(self):
-                message=self.message
-		self.message=''
-		self.readyToSend=False
-		return message
-
-	def display(self,text):
-		self.readyToDisplay=True
-		self.printme=text
+    def display(self,text):
+        self.readyToDisplay=True
+        self.printme=text
