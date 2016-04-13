b2c585b1d7b89c67500c327da070cd638d4a4e6e
Matthias Kastner <mail@matthias-kastner.de>
2016-04-13 14:00:13 +0200

    MKFlow main thread
    changed MKFlow to a threadable class

--- src/MKFlowMain.py
+++ src/MKFlowMain.py
@@ -1,62 +1,80 @@
 #!/usr/bin/env python
+import threading
+
 from MKFlowInput import MKFlowInput
 from MKFlowCommunication import MKFlowCommunication
 from MKDatabase import MKDatabase
 
-def main():
-    Input = MKFlowInput()
-    #Input.setLogFile('/home/matthias/Documents/programs/python/swnt-reactor/data/log/bridge/testing/log1.log')
-    Input.setBridge('/dev/ttyUSB2', '/dev/ttyUSB3')
-    Storage = MKFlowCommunication()
-    Database = MKDatabase()
-    while Input.isAlive():
-        try:
-            Message = Input.getMessage()
-            SubMessage = Message.getSubType()
-        except:
-            raise # reraise for debug reasons
-            break
-        else:
-            if not Message.isInvalid:
-                node = Message.getNode()
-                sequence = Message.getSequence()
-                Entity = Storage.Node(node).Sequence(sequence)
-                try:
-                    if Message.isError:
-                        Entity.setError(Message)
-                    elif Message.isStatus:
-                        Entity.setStatus(Message)
-                    elif Message.isSent:
-                        Entity.setAnswer(Message)
-                    elif Message.isSentStatus:
-                        Entity.setWriteRequest(Message)
-                    elif Message.isRequest:
-                        Entity.setReadRequest(Message)
-                    else:
-                        raise
-                except:
-                    print "--- something happened ---"
-                    try:
-                        Message.stdout()
-                        Submessage.stdout()
-                        Entity.reset()
-                    except:
-                        raise
-                else:
-                    # store if two messages are present in current Entity
-                    # reset Entity afterwards.
-                    try:
-                        Entity.output()
-                        Entity.save(Database)
-                    except ValueError:
-                        raise
-                        pass
-                    except:
-                        raise
-            else:
-                print "--- invalid ---"
-                SubMessage.stdoutShort(Message.stdoutShort())
-                pass
-main()
+class MKFlow():
+    def __init__(self, port1 = '/dev/ttyUSB2', port2 = '/dev/ttyUSB3', instrument = 0):
+        self.Input = MKFlowInput()
+        self.Storage = MKFlowCommunication()
+        self.Database = MKDatabase()
+        self.Input.setBridge(port1, port2)
+        #self.Input.setLogFile('/home/matthias/Documents/programs/python/swnt-reactor/data/log/bridge/testing/log1.log')
+        self.instrument = instrument
 
+    def start(self):
+        try:
+            self.thread = threading.Thread(target=self.loop)
+            self.thread.daemon = True
+            self.thread.start()
+        except:
+            self.Input.stop()
+            raise
+
+    def stop(self):
+        self.Input.stop()
+
+    def join(self):
+        self.thread.join()
+
+    def loop(self):
+        while self.Input.isAlive():
+            try:
+                Message = self.Input.getMessage()
+                SubMessage = Message.getSubType()
+            except:
+                break
+            else:
+                if not Message.isInvalid:
+                    node = Message.getNode()
+                    sequence = Message.getSequence()
+                    Entity = self.Storage.Node(node).Sequence(sequence)
+                    try:
+                        if Message.isError:
+                            Entity.setError(Message)
+                        elif Message.isStatus:
+                            Entity.setStatus(Message)
+                        elif Message.isSent:
+                            Entity.setAnswer(Message)
+                        elif Message.isSentStatus:
+                            Entity.setWriteRequest(Message)
+                        elif Message.isRequest:
+                            Entity.setReadRequest(Message)
+                        else:
+                            raise
+                    except:
+                        print "--- something happened ---"
+                        try:
+                            Message.stdout()
+                            Submessage.stdout()
+                            Entity.reset()
+                        except:
+                            raise
+                    else:
+                        # store if two messages are present in current Entity
+                        # reset Entity afterwards.
+                        try:
+                            #Entity.output()
+                            Entity.save(self.Database, self.instrument)
+                        except ValueError:
+                            raise
+                            pass
+                        except:
+                            raise
+                else:
+                    print "--- invalid ---"
+                    SubMessage.stdoutShort(Message.stdoutShort())
+                    pass
 
