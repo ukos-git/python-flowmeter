a23cd1ec267f86c41ce89bce2ea4fd1c92556192
Matthias Kastner <mail@matthias-kastner.de>
2016-04-18 15:03:21 +0200

    bash launcher
    more generic bash launcher depending on hostname

--- swnt-reactor-gui.sh
+++ swnt-reactor-gui.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-#python /home/pi/programs/swnt-reactor/src/MKTkinker.py
-python /home/matthias/Documents/programs/python/swnt-reactor/src/MKTkinker.py
+if [ "$(hostname)" == "raspberrypi" ]
+then
+    python /home/pi/programs/swnt-reactor/src/MKTkinker.py
+else
+    python /home/matthias/Documents/programs/python/swnt-reactor/src/MKTkinker.py
+fi
--- swnt-reactor-server.sh
+++ swnt-reactor-server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-python /home/pi/programs/swnt-reactor/src/MKMain.py
-#python /home/matthias/Documents/programs/python/swnt-reactor/src/MKtkinker.py
+if [ "$(hostname)" == "raspberrypi" ]
+then
+    python /home/pi/programs/swnt-reactor/src/MKMain.py
+else
+    python /home/matthias/Documents/programs/python/swnt-reactor/src/MKtkinker.py
+fi
