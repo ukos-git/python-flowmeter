c4b268484bc4048dc0efafb4aa8ae5a929f325a3
Matthias Kastner <mail@matthias-kastner.de>
2016-04-19 18:08:40 +0200

    fileName changes

--- swnt-reactor-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$(hostname)" == "raspberrypi" ]
-then
-    python /home/pi/programs/swnt-reactor/src/MKMain.py
-else
-    python /home/matthias/Documents/programs/python/swnt-reactor/src/MKtkinker.py
-fi
