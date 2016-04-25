d2f29c493d5b0f230fd0aa7fa51f248904651860
Matthias Kastner <mail@matthias-kastner.de>
2016-04-25 14:28:07 +0200

    new print function
    flush single character with print function

--- src/MKArduino.py
+++ src/MKArduino.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import threading
 import time
+import sys
 
 from MKDatabase import MKDatabase
 from MKParser import MKParser
@@ -15,6 +17,7 @@ class MKArduino():
         self.newFile = True
         self.setFile = False
         self.alive = False
+        self.sleeping = False
         self.debugging = False
 
     def debug(self):
@@ -44,30 +47,39 @@ class MKArduino():
 
     def loop(self):
         if self.debugging:
-            print "entering loop ..."
+            print('entering loop ...')
         while self.isAlive():
             serialReady = self.Serial.isReady()
             databaseReady = self.Database.isReady()
             if not serialReady and not databaseReady:
                 if self.debugging:
-                    print "sleeping ..."
+                    if not self.sleeping:
+                        print('sleeping .', end = '')
+                        self.sleeping = True
+                    else:
+                        print('.', end = '')
+                    sys.stdout.flush()
                 time.sleep(0.1)
             else:
                 if self.debugging:
+                    if self.sleeping:
+                        print('.', end = '\n')
+                        sys.stdout.flush()
+                        self.sleeping = False
                     if serialReady:
-                        print "serial ready with %i messages" % len(self.Serial.receiveBuffer)
+                        print('serial ready with %i messages' % len(self.Serial.receiveBuffer))
                     if databaseReady:
-                        print "database ready"
+                        print('database ready')
             if databaseReady:
                 if self.debugging:
-                    print "database ready ..."
+                    print('database ready ...')
                 self.Serial.send(self.Database.getMessage())
             if serialReady:
                 message = self.Serial.getMessage()
                 self.Parser.input(message)
                 oneline = self.Parser.oneline()
                 if self.debugging:
-                    print oneline
+                    print(oneline)
                 if not self.Parser.isHeadline() and self.Parser.getStatus():
                     setData = self.Database.setData(self.Parser.get(2), self.Parser.get(5), self.Parser.get(8), self.Parser.get(11))
                     setSP = self.Database.setSetpoint(self.Parser.get(3), self.Parser.get(6), self.Parser.get(9), self.Parser.get(12))
@@ -76,13 +88,13 @@ class MKArduino():
                         self.Serial.receive(message)
                 if self.Database.isRecording():
                     if self.newFile:
-                        print "starting new LogFile"
+                        print('starting new LogFile.')
                         self.Logfile = MKLogFileHandler('mkmain','log',True)
                         self.Logfile.open()
                         self.newFile = False
                         self.setFile = True
                     if self.setFile:
-                        print "set FileName in Database"
+                        print('set FileName in Database')
                         if self.Database.setLogFile(self.Logfile.getLogFile()):
                             self.setFile = False
                     self.Logfile.write(oneline)
