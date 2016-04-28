c9f4c3f460c1441a3092f23aa4d4efd9495937ba
Matthias Kastner <mail@matthias-kastner.de>
2016-04-28 09:41:37 +0200

    use pypy for better performance

--- client-gui.sh
+++ client-gui.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ "$(hostname)" == "raspberrypi" ]
 then
-    python /home/pi/programs/swnt-reactor/src/MKTkinker.py
+    pypy /home/pi/programs/swnt-reactor/src/MKTkinker.py
 fi
 if [ "$(hostname)" == "lab117" ]
 then
-    python /home/lab/programs/swnt-reactor/src/MKTkinker.py
+    pypy /home/lab/programs/swnt-reactor/src/MKTkinker.py
 fi
 if [ "$(hostname)" == "uk-work" ]
 then
-    python /home/matthias/Documents/programs/python/swnt-reactor/src/MKTkinker.py
+    pypy /home/matthias/Documents/programs/python/swnt-reactor/src/MKTkinker.py
 fi
