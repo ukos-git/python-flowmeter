2c482961aa168f504eea8ada7e96dfb9598e182a
Matthias Kastner <mail@matthias-kastner.de>
2017-02-23 11:58:49 +0100

    update scripts for second flowmeter
    second flow meter is for controlling argon. first is for ethanol.
    Both are bridged via a USB to RS232 adapter on
    ttyUSB0-->ttyUSB1 and ttyUSB3-->ttyUSB2

--- script/bridgeEtOH.sh
+++ script/bridgeEtOH.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 NOW=$(date +"%Y-%m-%d")
-socat -x /dev/ttyUSB2,raw,echo=0,b38400 /dev/ttyUSB3,raw,echo=0,b38400 2>>"log_bridgeEtOH_$NOW"& tail -f "log_bridgeEtOH_$NOW"
+# socat -x /dev/ttyUSB2,raw,echo=0,b38400 /dev/ttyUSB3,raw,echo=0,b38400 2>>"log_bridgeEtOH_$NOW"& tail -f "log_bridgeEtOH_$NOW"
+socat /dev/ttyUSB2,raw,echo=0,b38400 /dev/ttyUSB3,raw,echo=0,b38400
