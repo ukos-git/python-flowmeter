dcb655a2f034577d086b7bdd60078a7224293e52
Matthias Kastner <mail@matthias-kastner.de>
2017-02-24 14:58:13 +0100

    cleanup
    rsync is not needed as github takes care of this part

--- sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -AaXv ./src/*.py pi@132.187.77.177:/home/pi/programs/swnt-reactor/src
-rsync -AaXv ./src/*.py pi@132.187.77.184:/home/pi/programs/swnt-reactor/src
-rsync -AaXv ./src/*.py pi@132.187.77.181:/home/pi/programs/swnt-reactor/src
